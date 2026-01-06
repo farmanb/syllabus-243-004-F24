8ba44fbd2217f692639a35c5cafe9f9c10c8886d
blake-farman <blake.farman@gmail.com>
2026-01-06 15:48:58 -0600

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,113 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "frontmatter",
+  "level": "1",
+  "url": "frontmatter.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "  Blake Farman Mathematics and Statistics Louisiana Tech University        copyright   "
+},
+{
+  "id": "contact_information",
+  "level": "1",
+  "url": "contact_information.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Contact Information",
+  "body": " Contact Information      Professor  Dr. Blake Farman    Phone Number  318.257.2951    Email Address   bfarman@latech.edu    Website      Office  Nethken 235       Office Hours     Mon\/Wed\/Fri  12:30 PM  3:00 PM    Tues\/Thurs  1:00 PM  2:15 PM       Preferred Method of Communication  The best way to communicate with me during the semester is through email. I monitor my email during regular business hours and try to respond within one business day.    Official University Email Addresses  The University provides each student with an email address and all official course correspondence will only be conducted using official university email addresses.    Email Etiquette  When you send an email, your message should contain the following information   Subject  A few words that describe the content of your email along with the course identifier (Math 243 004)                   Salutation  A professional greeting such as Dear or Hello followed by the appropriate honorific and the recipient's last name (  Dr. X).   If the person you are addressing holds a doctorate ( a PhD ), then you should address the recipient as Dr. X, never Mr. X, Ms. X, or Mrs. . X.    If you are unsure, Professor X is generally a safe way to address a faculty member.       Body  A concise message that clearly expresses the purpose of your email.    Signature  A simple closing (e.g. Sincerely,  Thank you,  Best regards,  ) followed by your first and last name.   For your convenience, included below is an example email.   Sample Email  Subject: Meeting - Math 243 004                 Dear Dr. Farman,  I am having trouble with this week's homework. I have tried working through the examples in the text, but I am struggling to understand the concepts involved. Could we schedule a time to meet before the next class to discuss some of the concepts in more detail?  Thank you,  Jane Doe    "
+},
+{
+  "id": "course_information",
+  "level": "1",
+  "url": "course_information.html",
+  "type": "Section",
+  "number": "2",
+  "title": "Course Information",
+  "body": " Course Information   Course Description  Vectors, three-dimensional coordinates, double and triple integrals, vector valued functions and motion in space.    Course Prerequisites  A grade of C or better in MATH 242.    Student Learning Outcomes  The student will become proficient in vectors and the geometry of space, vector functions, and multiple integration. This proficiency will be demonstrated by satisfactorily completing a series of exams and homework assignments.    Course Topics  Topics include   Vectors and the Geometry of Space    Planes and Surfaces    Parameterization    Functions of Several Real Variables    Multiple Integration    Vector-Valued Functions       Instructional Methods  This course is offered as a face-to-face course. Learning will be facilitated through traditional lecture, group work\/activities, homework, and in-class assessments.   "
+},
+{
+  "id": "evaluation",
+  "level": "1",
+  "url": "evaluation.html",
+  "type": "Section",
+  "number": "3",
+  "title": "Evaluation",
+  "body": " Evaluation   Exams  There will be three in-class exams and a cumulative final exam. The exams are scheduled as follows:     Exam  Date    Exam 1  Thursday, September 29, 2024    Exam 2  Thursday, October 17, 2024    Exam 3  Thursday, November 7, 2024    Final Exam  November 13, 2024, 7 PM - 9 PM , IESB 318          The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.        Homework  Homework will be assigned regularly. The problems are chosen to highlight the core concepts from the material. Mastery of these homework sets serves as a good indicator for performance on assessments. As such, you should ensure that you fully understand the material on these homework sets; that is, upon completion of the homework set, you should be capable of completing similar problems without the aid of the text, a computer, or any other tools not available during an exam.    Weights  Grades will be calculated using the weights below.     Grade Category  Weight    Homework  10%    Exam 1  20%    Exam 2  20%    Exam 3  20%    Final Exam  30%       Grading Scale  Letter grades will be assigned based on the scale below.   Grading Scale    Average  Letter Grade     A     B     C     D     F      "
+},
+{
+  "id": "exams-4",
+  "level": "2",
+  "url": "evaluation.html#exams-4",
+  "type": "Warning",
+  "number": "3.1",
+  "title": "",
+  "body": "    The instructor reserves the right to modify the schedule of in class exams.    The final exam date and time are scheduled in advanced. Failure to appear at the final exam will result in a grade of zero for the exam.     "
+},
+{
+  "id": "policies",
+  "level": "1",
+  "url": "policies.html",
+  "type": "Section",
+  "number": "4",
+  "title": "Course Policies and Procedures",
+  "body": " Course Policies and Procedures   At a minimum, all policies stated in the current Louisiana Tech University Student Handbook should be followed. Additional class policies follow.    Textbook   Required Textbook   Calculus: Early Transcendentals , Edition, by James Stewart, Daniel Clegg, Saleem Watson. ISBN : 1-337-61392-4.     Attendance Policy  Per University Policy 2206 :    All students are expected to attend classes in which they are enrolled regularly and punctually. Failure to do so may jeopardize a student's scholastic standing and may lead to suspension from the College or University.  The student is responsible for making arrangements satisfactory to the instructor regarding absences including prior notification of planned absences, providing documentation if needed, and making up of examinations and work as required. For other emergency absences, the student shall submit excuses for class absences to the appropriate instructor within three class days following the student's return to their respective class.    As noted above, students are obligated to complete all assigned work promptly, to attend class regularly, and to participate in whatever class discussion may occur. Any behavior that interferes with the ability of other students to learn is unacceptable. If any of these become an issue, you will be asked to leave class for the day.  Your phone should be on silent and put away during class. Headphones should also be put away during class. Texting and browsing social media during class is rude and disrespectful. Please refrain from using your phone during class unless explicitly stated otherwise. If you are caught with a cell phone on your desk, in your lap, or anywhere in view while taking a quiz or exam, it will be treated as a case of academic dishonesty. Smart watches are also prohibited during exams; please put them away during those times.    Late Policy  Late work will not be accepted. Students who cannot submit work due to a University-related activity requiring an absence from class must arrange for work to be submitted before leaving.    Make-up Policy  In the event of one missed exam due to absence, the Final Exam can be used to replace the missing exam. Further missed exams will result in a grade of zero. If a University sponsored activity requires you to miss an exam, you must complete the exam in advance.    Academic Honor Code  In accordance with the Academic Honor Code , students pledge the following:    Being a student of higher standards, I pledge to embody the principles of academic integrity.    Any suspected case of academic dishonesty will be reported. If it is determined that academic misconduct has occurred, the student will receive a grade of F for the course and may be dismised from the University.   "
+},
+{
+  "id": "student_services",
+  "level": "1",
+  "url": "student_services.html",
+  "type": "Section",
+  "number": "5",
+  "title": "Student Services",
+  "body": " Student Services   Accommodations for Students with Disabilities  Students needing testing or classroom accommodations based on a disability must be registered with the Louisiana Tech University Office of Testing & Disability Services. Students are encouraged to discuss those needs with the instructor as soon as possible. If you do not have a current accommodation memo, visit the Office of Testing & Disability Services for assistance.    Counseling Services  Students have access confidential counseling services at no additional cost. Visit the Louisiana Tech Counseling Services website to learn more.    Non-Discrimination  Louisiana Tech University is committed to the principle of providing the opportunity for learning and development of all qualified citizens without regard to race, color, national origin, religion, age, sex, sexual orientation, marital status, or disability. Persons with concerns may contact the Louisiana Tech Director of Title IX Compliance:        Mortissa D. Harvey    Phone  318.257.5911    Office  Wyly Tower 1536    Email  mharvey@latech.edu      Section 504 is a federal law designed to protect the rights of individuals with disabilities in programs and activities that receive Federal financial assistance from the U.S. Department of Education. Persons with concerns may contact the Section 504 Coordinator:        Annie Jantz    Phone:  318.257.2445    Office:  Division of Student Affairs; Keeny Hall 305    Email:  ajantz@latech.edu        Emergency Notification System ( ENS )  All Louisiana Tech students are strongly encouraged to enroll and update their contact information in the Emergency Notification System. It takes just a few seconds to ensure you’re able to receive important text and voice alerts in the event of a campus emergency.    Canvas and Workday Help  Direct questions\/problems to the Computing Center Help Desk ( helpdesk@latech.edu or 318-257-5300).    BARC  Free tutoring sessions held by the BARC , Wyly Tower room 225, are open to anyone on a walk-in or Zoom-In basis. Sessions will begin the second week of classes. Hours can be found on their website .    Math Club  The math club hosts free, weekly Math & Music tutoring sessions. The math club also maintains a list of private tutors ; please contact them for more information. More information can be found on the Math Club website .   "
+},
+{
+  "id": "BARC-2",
+  "level": "2",
+  "url": "student_services.html#BARC-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "BARC "
+},
+{
+  "id": "math_club-2",
+  "level": "2",
+  "url": "student_services.html#math_club-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Math & Music private tutors "
+},
+{
+  "id": "schedule",
+  "level": "1",
+  "url": "schedule.html",
+  "type": "Section",
+  "number": "6",
+  "title": "Schedule",
+  "body": " Schedule   September 2 September 6            9\/2  9\/3  9\/4  9\/5  9\/6               Three-Dimensional Coordinate Systems ( 12.1)  Vectors ( 12.2)          September 9 September 13            9\/9  9\/10  9\/11  9\/12  9\/13        The Dot Product ( 12.3)     The Cross Product ( 12.4)  Curves Defined by Parametric Equations ( 10.1)          September 16 September 20            9\/16  9\/17  9\/18  9\/19  9\/20        Equations of Lines and Planes ( 12.5)       Conic Sections ( 10.5)  Cylinders and Quadric Surfaces ( 12.6)          September 23 September 27            9\/23  9\/24  9\/25  9\/26  9\/27        Review        Exam 1           September 30 October 4            9\/30  10\/1  10\/2  10\/3  10\/4        Functions of Several Variables ( 14.1)  Limits and Continuity ( 14.2)       Double Integrals over Rectangles ( 15.1)  Iterated Integrals and Fubini’s Theorem ( 15.2)          October 7 October 11            10\/7  10\/8  10\/9  10\/10  10\/11        Iterated Integrals and Fubini’s Theorem ( 15.2)  Polar Coordinates ( 10.3)  Double Integrals in Polar Coordinates ( 15.3)       Triple Integrals ( 15.6)          October 14 October 18            10\/14  10\/15  10\/16  10\/17  10\/18        Review        Exam 2           October 21 October 25            10\/21  10\/22  10\/23  10\/24  10\/25        Applications of Double Integrals ( 15.4)  Applications of Triple Integrals ( 15.6)  Triple Integrals in Cylindrical Coordinates ( 15.7)       Triple Integrals in Spherical Coordinates ( 15.8)     Last day to drop courses or resign with W grades        October 28 November 1            10\/28  10\/29  10\/30  10\/31  11\/1        Vector Functions and Space Curves ( 13.1)  Derivatives & Integrals Of Vector Functions ( 13.2)       Arc Length and Curvature ( 13.3)  Motion in Space: Velocity and Acceleration ( 13.4)          November 4 November 8            11\/4  11\/5  11\/6  11\/7  11\/8        Review        Exam 3           November 11 November 15            11\/11  11\/12  11\/13  11\/14  11\/15        Review     Final Exam  7 PM  8 PM  IESB 318      Last Day of Classes            The instructor reserves the right to modify the schedule as needed.    "
+},
+{
+  "id": "schedule-13-1",
+  "level": "2",
+  "url": "schedule.html#schedule-13-1",
+  "type": "Warning",
+  "number": "6.1",
+  "title": "",
+  "body": " The instructor reserves the right to modify the schedule as needed.  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
